8a7ff4d1ba918e65a60825a47e8cbed41fd8e1ec
Parinya Termkasipanich <40797988+parinyapt@users.noreply.github.com>
2023-10-26 21:34:48 +0700

    add init function in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"os"
+
+	"github.com/parinyapt/prinflix_backend/config"
+	"github.com/parinyapt/prinflix_backend/database"
+	"github.com/parinyapt/prinflix_backend/logger"
+	"github.com/parinyapt/prinflix_backend/routes"
+)
+
 func main() {
-	
+	config.InitializeDeployModeFlag()
+
+	logger.InitializeLogger(os.Getenv("DEPLOY_MODE"))
+	config.InitializeConfig()
+	database.InitializeDatabase()
+	routes.InitializeRoutes()
 }
