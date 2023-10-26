c61148c2baf9b952ffb49b1a6dffc528a599c8fd
Parinya Termkasipanich <40797988+parinyapt@users.noreply.github.com>
2023-10-27 00:24:07 +0700

    Add response model login and register

---
+++ model/handler/model_h_auth.go
@@ -0,0 +1,18 @@
+package modelHandler
+
+type RequestLogin struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type RequestRegister struct {
+	Name     string `json:"name" validate:"required,alpha_space,min=2,max=200"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8,max=100"`
+}
+
+type ResponseAccessToken struct {
+	TokenType            string `json:"token_type"`
+	AccessToken          string `json:"access_token"`
+	AccessTokenExpireIn  int    `json:"access_token_expire_in"`
+}
