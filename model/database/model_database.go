0bd3350a7e7b0c1f8b99425510c536f256a9805b
Parinya Termkasipanich <40797988+parinyapt@users.noreply.github.com>
2023-10-27 00:22:01 +0700

    Add auth session database design

--- model/database/model_database.go
+++ model/database/model_database.go
@@ -23,3 +23,14 @@ type Account struct {
 func (Account) TableName() string {
 	return utilsDatabase.GenerateTableName("account")
 }
+
+type AuthSession struct {
+	UUID        uuid.UUID `gorm:"column:auth_session_uuid;primary_key;not null"`
+	AccountUUID uuid.UUID `gorm:"column:account_uuid;not null"`
+	ExpiredAt   time.Time `gorm:"column:auth_session_expired_at;not null"`
+	CreatedAt   time.Time `gorm:"column:auth_session_created_at;not null"`
+}
+
+func (AuthSession) TableName() string {
+	return utilsDatabase.GenerateTableName("auth_session")
+}
