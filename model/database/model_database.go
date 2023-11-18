08ea97b849f363296baf3cc6464e4495e6bd3004
Parinya Termkasipanich <40797988+parinyapt@users.noreply.github.com>
2023-11-18 14:41:23 +0700

    Add database model

--- model/database/model_database.go
+++ model/database/model_database.go
@@ -35,3 +35,59 @@ type AuthSession struct {
 func (AuthSession) TableName() string {
 	return utilsDatabase.GenerateTableName("auth_session")
 }
+
+type AccountOauth struct {
+	AccountUUID uuid.UUID `gorm:"column:account_oauth_account_uuid;primary_key;not null"`
+	Provider    string    `gorm:"column:account_oauth_provider;primary_key;type:enum('line', 'google');not null"`
+	UserID      string    `gorm:"column:account_oauth_user_id;not null"`
+	CreatedAt   time.Time `gorm:"column:account_oauth_created_at;not null"`
+}
+
+func (AccountOauth) TableName() string {
+	return utilsDatabase.GenerateTableName("account_oauth")
+}
+
+type TemporaryCode struct {
+	UUID        uuid.UUID `gorm:"column:auth_temporary_code_uuid;type:uuid;primary_key;not null"`
+	AccountUUID uuid.UUID `gorm:"column:auth_temporary_code_account_uuid;not null"`
+	Type        string    `gorm:"column:auth_temporary_code_type;type:enum('email_verification', 'password_reset', 'oauth_state');not null"`
+	CreatedAt   time.Time `gorm:"column:auth_temporary_code_created_at;type:timestamp;not null"`
+}
+
+func (TemporaryCode) TableName() string {
+	return utilsDatabase.GenerateTableName("temporary_code")
+}
+
+type Movie struct {
+	UUID        uuid.UUID `gorm:"column:movie_uuid;primary_key;not null"`
+	CategoryID  uint      `gorm:"column:movie_category_id;not null"`
+	Title       string    `gorm:"column:movie_title;not null"`
+	Description string    `gorm:"column:movie_description;not null"`
+	CreatedAt   time.Time `gorm:"column:movie_created_at;not null"`
+	UpdatedAt   time.Time `gorm:"column:movie_updated_at;not null"`
+}
+
+func (Movie) TableName() string {
+	return utilsDatabase.GenerateTableName("movie")
+}
+
+type MovieCategory struct {
+	ID        uint      `gorm:"column:movie_category_id;primary_key;not null"`
+	Name      string    `gorm:"column:movie_category;not null"`
+	CreatedAt time.Time `gorm:"column:movie_category_created_at;not null"`
+	UpdatedAt time.Time `gorm:"column:movie_category_updated_at;not null"`
+}
+
+func (MovieCategory) TableName() string {
+	return utilsDatabase.GenerateTableName("movie_category")
+}
+
+type FavoriteMovie struct {
+	AccountUUID uuid.UUID `gorm:"column:favorite_movie_account_uuid;primary_key;not null"`
+	MovieUUID   uuid.UUID `gorm:"column:favorite_movie_movie_uuid;primary_key;not null"`
+	CreatedAt   time.Time `gorm:"column:favorite_movie_created_at;not null"`
+}
+
+func (FavoriteMovie) TableName() string {
+	return utilsDatabase.GenerateTableName("favorite_movie")
+}
