194f99c9a169a03b5e5e931ac412a5e89b4c7039
Parinya Termkasipanich <40797988+parinyapt@users.noreply.github.com>
2023-11-28 01:00:12 +0700

    Fix Bug

--- middleware/external_middleware.go
+++ middleware/external_middleware.go
@@ -174,6 +174,6 @@ func WatchSessionCheck(c *gin.Context) {
 		return
 	}
 
-	c.Set("WATCHSESSION_MOVIE_UUID", checkWatchSession.AccountUUID.String())
+	c.Set("WATCHSESSION_MOVIE_UUID", checkWatchSession.MovieUUID.String())
 	c.Next()
 }
